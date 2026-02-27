a5378ff39bc3091ddd72f9851ad9b03e4f0ce6e6
zack-das <izackdas@gmai.com>
2026-02-27 20:36:20 +0300

    Resolve merge conflict in _config.js

--- _config.js
+++ _config.js
@@ -2,14 +2,13 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-<<<<<<< HEAD
-    production: 'mongodb+srv://izackdas_db_user:zQ3yoggbSoeVJZba@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://izackdas_db_user:zQ3yoggbSoeVJZba@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://izackdas_db_user:zQ3yoggbSoeVJZba@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
-=======
+// <<<<<<< HEAD
+//     production: 'mongodb+srv://izackdas_db_user:zQ3yoggbSoeVJZba@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
+//     development: 'mongodb+srv://izackdas_db_user:zQ3yoggbSoeVJZba@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+//     test: 'mongodb+srv://izackdas_db_user:zQ3yoggbSoeVJZba@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+// =======
     production: 'mongodb+srv://izackdas_db_user:9wFuCdAggY2z3qWO@cluster0.ootlczi.mongodb.net/darkroom?retryWrites=true&w=majority',
     development: 'mongodb+srv://izackdas_db_user:9wFuCdAggY2z3qWO@cluster0.ootlczi.mongodb.net/darkroom?retryWrites=true&w=majority',
     test: 'mongodb://127.0.0.1:27017/darkroom-test',
->>>>>>> test
 }
 module.exports = config;
